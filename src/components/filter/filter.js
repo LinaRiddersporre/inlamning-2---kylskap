19eeee3a2f6a5542ac32b906c809145371584e8d
Lina <linariddersporre@gmail.com>
2022-10-30 22:27:20 +0100

    key fix

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -5,9 +5,9 @@ class Filter extends React.Component {
     render() {
         return (
             <div>
-                {this.props.categories.map((value) => {
+                {this.props.categories.map((value, index) => {
                     return (
-                        <button onClick={() => { this.props.filterWord(value) }}> {value} </button>)
+                        <button key={index} onClick={() => { this.props.filterWord(value) }}> {value} </button>)
                 })}
             </div>
 
